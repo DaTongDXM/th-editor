4fc5ebfeeddfafb08e0e196754ce4608433a6453
wuxudong <953909305@qq.com>
2023-11-05 18:02:54 +0800

    ✨ feat: 调整loading页面样式

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -2,7 +2,7 @@
  * @Author: wuxudong wuxudong@zbnsec.com
  * @Date: 2022-11-15 01:13:46
  * @LastEditors: wuxudong 953909305@qq.com
- * @LastEditTime: 2023-11-05 17:20:34
+ * @LastEditTime: 2023-11-05 18:00:50
  * @Description:The editor container contains the canvas , toolbar and attribute
  */
 import React, { useEffect, useState } from 'react';
@@ -37,7 +37,7 @@ const EditorCore: React.FC<EditorCoreProps & { editorRef: any }> = ({ onClick })
       <div className={`mask-container-item left ${loading ? '' : 'hidden'}`}></div>
       <Loading loading={loading}>Loading...</Loading>
       <div className={`mask-container-item right ${loading ? '' : 'hidden'}`}></div>
-      <Model menuShow />
+      <Model menuShow={false} />
       <div id='render-container' className='main-container'></div>
     </>
   );

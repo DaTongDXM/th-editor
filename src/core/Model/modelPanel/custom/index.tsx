04a19408a2302be429f0bd8ac8f7ef123abf5f89
wuxudong <953909305@qq.com>
2023-11-22 20:10:03 +0800

    ✨ feat: 自定义模型组件头部

--- src/core/Model/modelPanel/custom/index.tsx
+++ src/core/Model/modelPanel/custom/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import './index.scss';
+import { Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 const CustomModel: React.FC<any> = () => {
-  return <>custom</>;
+  return (
+    <div className='custom-container'>
+      <div className='toolbar'>
+        <i className='iconfont th-liebiaoqiehuan'></i>
+
+        <i className='iconfont th-tianjia'></i>
+      </div>
+      <div className='container'></div>
+    </div>
+  );
 };
 export default CustomModel;

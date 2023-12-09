3cdc82db6975eeba413b5881a56eda8760bf307f
武 旭东 <wuxudong@zbnsec.com>
2023-12-09 20:05:40 +0800

    Revert "🐞 fix: 调整网格位置"
    This reverts commit 70c62747c5074a8793bde37995287bda733358b9.

--- src/three/Events.ts
+++ src/three/Events.ts
@@ -1,6 +1,6 @@
 /*
  * @Author: wuxudong 953909305@qq.com
- * @LastEditors: 武 旭东 wuxudong@zbnsec.com
+ * @LastEditors: wuxudong 953909305@qq.com
  * @Description:事件类
  */
 import { Camera, EventDispatcher, Raycaster, Scene, Vector2 } from 'three';
